13ab10d000e1c79442c6d9064cc71bac2d80c131
syntheticfeather <2391317090@qq.com>
2024-12-12 20:10:17 +0800

    镜头跟随与船只移动脚本

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public static CameraFollow instance;
+    public GameObject Target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
+        instance.gameObject.transform.position = Target.transform.position + new Vector3(0,0,-10);
     }
 }
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//玩家运动脚本
+//采用了AD控制方向，WS控制移动的运动原理。
+public class PlayerMovement : MonoBehaviour
+{
+    public static PlayerMovement Instance;
+    public Rigidbody2D rb;
+    public float Speed;
+    public float R_Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Input.GetAxis("Vertical") * transform.up * Speed;
+        rb.angularVelocity = Input.GetAxis("Horizontal") * R_Speed * -1;
+    }
+}
