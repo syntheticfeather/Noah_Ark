b6398843c9ea363b801e0e86bccddc176abd54d6
syntheticfeather <2391317090@qq.com>
2025-01-24 18:19:21 +0800

    横扫（）

--- Assets/Scripts/Enemy/Boss/Boss.cs
+++ Assets/Scripts/Enemy/Boss/Boss.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     public static Boss Instance;
-    public int StrikeDelayTime;
-    public int StrikeBetTime;
-    public float StrikeCounter;
-    public GameObject StrikeArea;
-    public GameObject CurStrikeArea;
-    public GameObject StrikeATK;
-
     private GameObject Player;
     // Start is called before the first frame update
     void Start()
@@ -21,16 +14,27 @@ public class Boss : MonoBehaviour
         Instance = this;
     }
 
+
     // Update is called once per frame
     void Update()
     {
         StrikeCounter -= Time.deltaTime;
+        SweepCounter -= Time.deltaTime;
         if (StrikeCounter < 0)
         {
             StartCoroutine(DelayStrike());
         }
+        if (SweepCounter < 0)
+        {
+            StartCoroutine(DelaySweep());
+        }
     }
-
+    public int StrikeDelayTime;
+    public int StrikeBetTime;
+    public float StrikeCounter;
+    public GameObject StrikeArea;
+    public GameObject CurStrikeArea;
+    public GameObject StrikeATK;
     // Í»´Ì
     IEnumerator DelayStrike()
     {   
@@ -40,9 +44,28 @@ public class Boss : MonoBehaviour
         yield return new WaitForSeconds(StrikeDelayTime);
         Strike();               
     }
-
     public void Strike()
     {
         Instantiate(StrikeATK, CurStrikeArea.transform.position, Quaternion.identity).SetActive(true);
+    }
+
+    public int SweepDelayTime;
+    public int SweepBetTime;
+    public float SweepCounter;
+    public GameObject SweepArea;
+    public GameObject CurSweepArea;
+    public GameObject SweepATK;
+    // Í»´Ì
+    IEnumerator DelaySweep()
+    {
+        SweepCounter = SweepBetTime + SweepDelayTime;
+        CurSweepArea = Instantiate(SweepArea, Player.transform.position, Quaternion.identity);
+        CurSweepArea.SetActive(true);
+        yield return new WaitForSeconds(SweepDelayTime);
+        Sweep();
+    }
+    public void Sweep()
+    {
+        Instantiate(SweepATK, CurSweepArea.transform.position, Quaternion.identity).SetActive(true);
     }
 }
