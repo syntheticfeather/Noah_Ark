7683fcbffa0ad6f88fe4d3e8972ae91b5b51ce37
Saki-174 <18381601561@163.com>
2025-01-25 17:48:15 +0800

    按钮逻辑完善

--- Assets/Scripts/UI/CharacterPanelManager.cs
+++ Assets/Scripts/UI/CharacterPanelManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,7 @@ public class CharacterPanelController : MonoBehaviour
 {
     public GameObject characterPanel;
     public bool isPanelActive = false;
+    public List<GameObject> list = new List<GameObject>();// 动物属性面板
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
@@ -27,4 +29,30 @@ public class CharacterPanelController : MonoBehaviour
         }
         
     }
+    // 点击按钮弹出动物属性面板
+    public void OnClickButton(Button button)
+    {
+        // 按钮父物体的transform
+        Transform parentTransform = button.transform.parent;
+        // 该按钮对应的Panel的transform
+        Transform panel;
+
+        // 先禁用所有属性面板
+        foreach (GameObject gameObject in list)
+        {
+            gameObject.SetActive(false);
+        }
+        if (parentTransform != null)
+        {
+            // 获取到按钮对应的Panel                     
+            panel = parentTransform.Find("Panel");
+            if (panel != null)
+            {
+                // 启用特定面板
+                panel.gameObject.SetActive(true);
+            }
+            
+        }                
+    }
+    
 }
