11751c88ad636cad87849d5ba970cd0d3ffc4267
syntheticfeather <2391317090@qq.com>
2025-01-31 11:17:43 +0800

    时间显示

--- Assets/Scripts/Player/Ark/Navigator.cs
+++ Assets/Scripts/Player/Ark/Navigator.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Navigator : MonoBehaviour
 {
+    public float Rate;
     public static Navigator instance;
-
-    public bool IsUsing;  
+    public GameObject ArkLight;
+    public bool IsUsing;
+    public bool Flag = false;
     void Start()
     {
         instance = this;
@@ -15,9 +18,11 @@ public class Navigator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (IsUsing)
+        if (IsUsing && !Flag)
         {
             //显示UI
+            Flag = true;
+            ArkLight.GetComponent<Light2D>().pointLightOuterRadius *= Rate;
         }
     }
 }
