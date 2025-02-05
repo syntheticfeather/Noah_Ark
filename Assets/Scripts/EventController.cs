12978032cfb336e2ee7b05a742d698fe4d8fe26a
syntheticfeather <2391317090@qq.com>
2025-02-05 12:41:28 +0800

    敌怪全局生成逻辑完善

--- Assets/Scripts/EventController.cs
+++ Assets/Scripts/EventController.cs
@@ -14,7 +14,6 @@ public class EventController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
